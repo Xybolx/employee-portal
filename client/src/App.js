a54ae69beb91f5fe3e9411174c3377b23666a013
Xybolx <xybolx@gmail.com>
2019-07-12 04:34:17 -0700

    production build

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,13 @@ const App = () => {
                 )
             )} />
             <Route exact path='/logout' component={LogOut} />
-            <Route exact path="/entry" component={Entry} />
+            <Route exact path="/entry" render={() => (
+              value.user !== null && value.user !== "" ? (
+                <Entry />
+                ) : (
+                  <Redirect to="/logout" />
+                )
+            )} />
             <Route exact path="/edit" render={() => (
               value.user !== null && value.user !== "" ? (
                 <Edit />
--- client/src/pages/entry.js
+++ client/src/pages/entry.js
@@ -56,7 +56,7 @@ const Entry = () => {
 
     return (
         <div>
-            {/* <PortalNav /> */}
+            <PortalNav />
             <UseIdleTimer />
             <Col>
                 <Title />
